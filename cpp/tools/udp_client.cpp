da8a141483c2b52edd51808d5f56f7eb4c9251b5
Peter Mitrano <mitranopeter@gmail.com>
2017-11-14 21:48:36 -0500

    add time stamping and struct message passing

--- cpp/tools/udp_client.cpp
+++ cpp/tools/udp_client.cpp
@@ -6,6 +6,7 @@
 #include <cstring>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <sys/time.h>
 
 #include "udp.h"
 
@@ -25,35 +26,70 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  struct sockaddr_in remaddr;
-  memset((char *) &remaddr, 0, sizeof(remaddr));
-  remaddr.sin_family = AF_INET;
-  remaddr.sin_port = htons(kPort);
-  if (inet_aton("127.0.0.1", &remaddr.sin_addr)==0) {
-    fprintf(stderr, "inet_aton() failed\n");
-    exit(1);
-  }
-
-  constexpr size_t BUFLEN = 1024;
   socklen_t address_length = sizeof(struct sockaddr_in);
-  char buf[BUFLEN];
-  ssize_t recvlen;
-  for (int i=0; i < 10; i++) {
-    sprintf(buf, "This is packet %d", i);
-    if (sendto(socket_fd, buf, strlen(buf), 0, (struct sockaddr *)&remaddr, address_length)==-1) {
-      perror("sendto");
-      exit(1);
+
+  struct sockaddr_in client_address = {0};
+  client_address.sin_family = AF_INET;
+  client_address.sin_port = htons(0);
+  client_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if (bind(socket_fd, (struct sockaddr *) &client_address, address_length) < 0) {
+    std::cerr << "bind failed: [" << strerror(errno) << "]" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  struct timeval tv = {};
+  tv.tv_sec = 0;
+  tv.tv_usec = 500000;
+  if (setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+    std::cerr << "setting socket timeout failed : [" << strerror(errno) << "]" << std::endl;
+  }
+
+  // look up hostname
+  struct hostent *hp;
+  hp = gethostbyname(hostname);
+  if (hp == nullptr) {
+    std::cerr << "gethostbyname failed: [" << strerror(errno) << "]" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  char *host_ip = hp->h_addr_list[0];
+  printf("Found %s at IP %d.%d.%d.%d\n", hostname, host_ip[0], host_ip[1], host_ip[2], host_ip[3]);
+
+  struct sockaddr_in server_address = {0};
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(kPort);
+  memcpy((void *) &server_address.sin_addr, hp->h_addr_list[0], hp->h_length);
+
+  struct sockaddr_in response_address = {0};
+
+  for (size_t i = 0; i < 10; ++i) {
+    usleep(20000);
+
+    data_t data = {};
+    data.left_encoder_rate = 1;
+    data.right_encoder_rate = 2;
+    struct timeval tv = {};
+    gettimeofday(&tv, nullptr);
+    data.rio_send_time_s = timeval_to_sec(tv);
+
+    if (sendto(socket_fd, (uint8_t *) &data, data_t_size, 0, (struct sockaddr *) &server_address, address_length) < 0) {
+      std::cerr << "sendto failed: [" << strerror(errno) << "]" << std::endl;
     }
-    /* now receive an acknowledgement from the server */
-    recvlen = recvfrom(socket_fd, buf, BUFLEN, 0, (struct sockaddr *)&remaddr, &address_length);
-    if (recvlen >= 0) {
-      buf[recvlen] = 0;	/* expect a printable string - terminate it */
-      printf("received message: \"%s\"\n", buf);
+
+    ssize_t recvlen =
+        recvfrom(socket_fd, (uint8_t *) &data, data_t_size, 0, (struct sockaddr *) &response_address, &address_length);
+
+    if (recvlen != data_t_size) {
+      printf("received %zd bytes, expected %zu bytes\n", recvlen, data_t_size);
+    } else {
+      print_data_t(data);
     }
   }
-  close(socket_fd);
-  return 0;
 
+  close(socket_fd);
+
+  return EXIT_SUCCESS;
 }
 
 void show_help() {
--- cpp/tools/udp_server.cpp
+++ cpp/tools/udp_server.cpp
@@ -17,8 +17,7 @@ int main(int argc, char *argv[]) {
 
   socklen_t address_length = sizeof(struct sockaddr_in);
 
-  struct sockaddr_in addr;
-  memset((char *)&addr, 0, sizeof(addr));
+  struct sockaddr_in addr = {0};
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = htonl(INADDR_ANY);
   addr.sin_port = htons(kPort);
@@ -28,22 +27,22 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  uint8_t buf[kBufferSize];
-  struct sockaddr_in remote_address;
+  struct sockaddr_in remote_address = {0};
   bool done = false;
-  struct timeval server_time;
+  struct timeval server_time = {};
+  data_t data;
   while (!done) {
-    ssize_t recvlen = recvfrom(socket_fd, buf, kBufferSize, 0, (struct sockaddr *) &remote_address, &address_length);
+    ssize_t recvlen =
+        recvfrom(socket_fd, (uint8_t *) &data, data_t_size, 0, (struct sockaddr *) &remote_address, &address_length);
 
-    printf("received %zd bytes\n", recvlen);
-    if (recvlen > 0) {
-      gettimeofday(&server_time, nullptr);
-      char *time_bytes = reinterpret_cast<char *>(&server_time);
-      printf("received message: \"%s\". Responding with server timestamp\n", buf);
-      if (sendto(socket_fd, time_bytes, sizeof(struct timeval), 0, (struct sockaddr *) &remote_address, address_length)
-          < 0) {
-        std::cerr << "sendto failed: [" << strerror(errno) << "]" << std::endl;
-      }
+    if (recvlen != data_t_size) {
+      printf("received %zd bytes, expected %zu bytes\n", recvlen, data_t_size);
+    }
+
+    gettimeofday(&server_time, nullptr);
+    data.tk1_recv_time_s = timeval_to_sec(server_time);
+    if (sendto(socket_fd, (uint8_t *) &data, data_t_size, 0, (struct sockaddr *) &remote_address, address_length) < 0) {
+      std::cerr << "sendto failed: [" << strerror(errno) << "]" << std::endl;
     }
   }
 
