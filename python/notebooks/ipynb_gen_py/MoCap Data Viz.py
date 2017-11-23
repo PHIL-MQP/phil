61bdf968de98db2d41ede00a0a2ffa887ed7f8b2
Peter Mitrano <mitranopeter@gmail.com>
2017-11-23 01:22:22 -0500

    Work on mocap data visuals
     - demo getting x/y from accelerometer and comparing to mocap
     - looks like IMU didn't record the whole thing
     - demonstrates the need for scaling and bias factors

--- python/notebooks/ipynb_gen_py/MoCap Data Viz.py
+++ python/notebooks/ipynb_gen_py/MoCap Data Viz.py
@@ -1,14 +1,14 @@
 
 # coding: utf-8
 
-# In[4]:
+# In[216]:
 
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
 
-# In[75]:
+# In[217]:
 
 data_dir = "../../recorded_sensor_data/mocap_11_17_18-00-00/"
 imu_file = data_dir + "mocap_imu_encoder_data.csv"
@@ -27,7 +27,7 @@ next(mocap_reader)
 imu_data = []
 for imu_row in imu_reader:
     data = [float(d) for d in imu_row]
-    if len(data) > 0 and data[-1] > 1800:
+    if len(data) > 0 and data[-1] > 1800: # hack to skip first round of data
         imu_data.append(data)
 imu_data = np.array(imu_data)
 
@@ -39,7 +39,7 @@ for mocap_row in mocap_reader:
 mocap_data = np.array(mocap_data)
 
 
-# In[76]:
+# In[218]:
 
 def yawdiff(y1, y2):
     diff = y2 - y1
@@ -50,13 +50,13 @@ def yawdiff(y1, y2):
     return diff;
 
 
-# In[77]:
+# In[219]:
 
 print("Seconds of IMU data recorded: ", imu_data[-1][-1] - imu_data[0][-1])
 print("Seconds of MoCap recorded:", len(mocap_data) / 100)
 
 
-# In[78]:
+# In[5]:
 
 plt.plot(mocap_data[:,2], label="rx")
 plt.plot(mocap_data[:,3], label="ry")
@@ -71,7 +71,7 @@ plt.legend()
 plt.show()
 
 
-# In[103]:
+# In[65]:
 
 states = np.ndarray((mocap_data.shape[0], 9))
 state = np.zeros(9)
@@ -86,20 +86,125 @@ for mocap_idx in range(1, len(mocap_data)):
     tz = data[7]
     state[0] = tx
     state[1] = ty
-    state[2] += yawdiff(rz, last_data[4]) # handles wrap-around
+    drz = yawdiff(rz, last_data[4]) # handles wrap-around
+    state[2] += drz
     states[mocap_idx] = state
 
 
-# In[116]:
+# In[214]:
 
+plt.figure(figsize=(10,10))
 plt.plot(states[:,2], label='theta')
 plt.legend()
 plt.title("Unwrapped Angle")
-
-plt.figure()
-plt.scatter(states[:,0], states[:,1], marker='.', s=1, color='r')
 plt.show()
 
+
+# In[215]:
+
+plt.figure(figsize=(10,10))
+plt.scatter(states[:,0], states[:,1], marker='.', s=1, color='r')
+plt.axis("square")
+plt.show()
+
+
+# In[80]:
+
+plt.plot(imu_data[:,0], label="IMU x")
+plt.plot(imu_data[:,1], label="IMU y")
+plt.plot(imu_data[:,2], label="IMU z")
+plt.title("IMU Data")
+plt.legend()
+plt.show()
+
+
+# In[81]:
+
+plt.plot(imu_data[:,3], label="Gyro x")
+plt.plot(imu_data[:,4], label="Gyro y")
+plt.plot(imu_data[:,5], label="Gyro z")
+plt.title("Gyro Data")
+plt.legend()
+plt.show()
+
+
+# In[117]:
+
+means = np.mean(imu_data,axis=0)
+print("Average Accel X value:", means[0])
+print("Average Accel Y value:", means[1])
+print("Average Accel Z value:", means[2])
+
+
+# In[91]:
+
+yaws = []
+yaw = 0
+last_t = imu_data[0][-1]
+for data in imu_data:
+    gyro_z = data[5]
+    dt_s = data[-1] - last_t
+    yaw += dt_s * gyro_z
+    yaws.append(yaw)
+    last_t = data[-1]
+
+
+# In[92]:
+
+plt.plot(yaws, label="integrated gyro")
+plt.show()
+
+
+# In[221]:
+
+x = 0
+y = 0
+vx = 0
+vy = 0
+last_t = imu_data[0][-1]
+xs = []
+ys = []
+vxs = []
+vys = []
+x_bias = 0 #.0385
+y_bias = 0 #.041
+x_scale = 1000
+y_scale = 1000
+for data in imu_data:
+    ax = (data[0] - x_bias) * x_scale
+    ay = (data[1] - y_bias) * y_scale
+    dt_s = data[-1] - last_t
+    vx += ax * dt_s
+    vy += ay * dt_s
+    x += vx * dt_s + 0.5 * ax * dt_s ** 2
+    y += vy * dt_s + 0.5 * ay * dt_s ** 2
+    last_t = data[-1]
+    vxs.append(vx)
+    vys.append(vy)
+    xs.append(x)
+    ys.append(y)
+
+
+# In[222]:
+
+plt.plot(vxs, label="vxs")
+plt.plot(vys, label="vys")
+plt.title("velocities from integrating accelerometer")
+plt.legend()
+plt.show()
+
+
+# In[223]:
+
+plt.figure(figsize=(10,10))
+plt.scatter(xs, ys, marker='.', s=1, color='b', label='Accelerometer')
+plt.scatter(states[:,0], states[:,1], marker='.', s=1, color='r', label='MoCap')
+plt.title("Accelerometer versus MoCap")
+plt.legend()
+plt.show()
+
+
+# ## Find X/Y accelerometer scales & biases that make the IMU data closest to the other position data
 
 # In[ ]:
 
