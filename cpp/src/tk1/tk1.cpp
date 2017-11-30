5b5c2623caaf53bbf4a12e722bcc9bd717c51b71
Peter Mitrano <mitranopeter@gmail.com>
2017-11-29 21:39:58 -0500

    start writing main TK1 program
     - also remove dorothys executable that has no main

--- cpp/src/tk1/tk1.cpp
+++ cpp/src/tk1/tk1.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <cstring>
+#include <cscore.h>
+#include <opencv2/core/mat.hpp>
+#include "../../include/phil/common/udp.h"
 
 int main(int argc, char **argv) {
   std::cout << "tk1 main program..." << std::endl;
+
+  // camera server
+  cs::UsbCamera camera{"usbcam", 0};
+  camera.SetVideoMode(cs::VideoMode::kMJPEG, 320, 240, 30);
+  cs::MjpegServer mjpegServer{"httpserver", 8081};
+  mjpegServer.SetSource(camera);
+  cs::CvSink sink{"sink"};
+  sink.SetSource(camera);
+
+  // comms with the roborio
+  phil::UDPServer server;
+
+  cv::Mat frame;
+  bool done = false;
+  while (!done) {
+    // read some sensor data from roborio
+    phil::data_t data = {0};
+    ssize_t bytes_received = server.Read(reinterpret_cast<uint8_t *>(&data), phil::data_t_size);
+
+    if (bytes_received != phil::data_t_size) {
+      std::cerr << "bytes does not match data_t_sze: [" << strerror(errno) << "]" << std::endl;
+      continue;
+    }
+
+    // grab latest camera frame
+    uint64_t time = sink.GrabFrame(frame);
+  }
+
   return 0;
 }
