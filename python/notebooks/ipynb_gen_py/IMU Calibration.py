3c889252a9db8c50d659cfc2370751454fef5f4e
Peter Mitrano <mitranopeter@gmail.com>
2017-12-05 19:59:39 -0500

    numerical approximations to accelerometer loss works

--- python/notebooks/ipynb_gen_py/IMU Calibration.py
+++ python/notebooks/ipynb_gen_py/IMU Calibration.py
@@ -8,6 +8,7 @@
 # In[1]:
 
 import matplotlib.pyplot as plot
+from scipy import misc
 import numpy as np
 import csv
 import sys
@@ -48,123 +49,42 @@ print("sample variance:", sample_variance)
 
 # ## Setting up the Jacobians
 # 
-# In order to run the LM minimization, you need to define the jacobian of the error you want to minimize with respect to the parameters.
-# For the first LM run, our error tern is $e={\lVert g\rVert}^2-{\lVert T^aK^a(a^S+b^a)\rVert}^2$. The parameters here are $[\alpha_{yz}, \alpha_{zy}, \alpha_{zx}, s^a_x, s^a_y, s^a_z, b^a_x, b^a_y, b^a_z]$, which means the jacobian will look like this:
-# 
-# $$\begin{bmatrix}
-# \frac{\partial e_1}{\partial \alpha_{yz}} & \frac{\partial e_1}{\partial \alpha_{zy}} & \frac{\partial e_1}{\partial \alpha_{zx}} & \frac{\partial e_1}{\partial s^a_x} & \frac{\partial e_1}{\partial s^a_y} & \frac{\partial e_1}{\partial s^a_z} & \frac{\partial e_1}{\partial b^a_x} & \frac{\partial e_1}{\partial b^a_y} & \frac{\partial e_1}{\partial b^a_z} \\
-# \vdots & \vdots & \vdots & \vdots & \vdots & \vdots & \vdots & \vdots & \vdots & \\
-# \frac{\partial e_M}{\partial \alpha_{yz}} & \frac{\partial e_M}{\partial \alpha_{zy}} & \frac{\partial e_M}{\partial \alpha_{zx}} & \frac{\partial e_M}{\partial s^a_x} & \frac{\partial e_M}{\partial s^a_y} & \frac{\partial e_M}{\partial s^a_z} & \frac{\partial e_M}{\partial b^a_x} & \frac{\partial e_M}{\partial b^a_y} & \frac{\partial e_M}{\partial b^a_z} \\
-# \end{bmatrix}$$
-# 
-# We can solve for these analytically.
-# 
-# $$\begin{align}
-# \frac{\partial e_i}{\partial \alpha_{yz}} &= \frac{\partial}{\partial \alpha_{yz}}\Big({\lVert g\rVert}^2-{\lVert T^aK^a(a^S+b^a)\rVert}^2\Big) \\
-#  &= \frac{\partial}{\partial \alpha_{yz}}{\lVert g\rVert}^2-\frac{\partial}{\partial \alpha_{yz}}{\lVert T^aK^a(a^S+b^a)\rVert}^2 \\
-#  &= -\frac{\partial}{\partial \alpha_{yz}}{\lVert T^aK^a(a^S+b^a)\rVert}^2 \\
-#  &= -\frac{\partial}{\partial \alpha_{yz}}\sqrt{\sum{\Big(T^aK^a(a^S+b^a)\Big)^2}}^2 \\
-#  &= -\frac{\partial}{\partial \alpha_{yz}}\sum{\Big(T^aK^a(a^S+b^a)\Big)^2} \\
-#  &= -\frac{\partial}{\partial \alpha_{yz}}\sum{\Bigg(\begin{bmatrix}1&-\alpha_{yz}&\alpha_{zy}\\0&1&-\alpha_{zx}\\0&0&1\\\end{bmatrix}\begin{bmatrix}s_x^a&0&0\\0&s_y^a&0\\0&0&s_z^a\\\end{bmatrix}\Bigg(\begin{bmatrix}a_{x,i}^s\\a_{y,i}^s\\a_{z,i}^s\end{bmatrix}+\begin{bmatrix}b_x^a\\b_y^a\\b_z^a\end{bmatrix}\Bigg)\Bigg)^2} \\
-#  &= -\frac{\partial}{\partial \alpha_{yz}}\sum{\Bigg(\begin{bmatrix}1&-\alpha_{yz}&\alpha_{zy}\\0&1&-\alpha_{zx}\\0&0&1\\\end{bmatrix}\begin{bmatrix}s_x^a&0&0\\0&s_y^a&0\\0&0&s_z^a\\\end{bmatrix}\begin{bmatrix}a_{x,i}^s+b_x^a\\a_{y,i}^s+b_y^a\\a_{z,i}^s+b_z^a\end{bmatrix}\Bigg)^2} \\
-#  &= -\frac{\partial}{\partial \alpha_{yz}}\sum{\Bigg(\begin{bmatrix}1&-\alpha_{yz}&\alpha_{zy}\\0&1&-\alpha_{zx}\\0&0&1\\\end{bmatrix}\begin{bmatrix}s_x^a(a_{x,i}^s+b_x^a)\\s_y^a(a_{y,i}^s+b_y^a)\\s_z^a(a_{z,i}^s+b_z^a)\end{bmatrix}\Bigg)^2} \\
-#  &= -\frac{\partial}{\partial \alpha_{yz}}\sum{\Bigg(\begin{bmatrix}s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\\s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\\s_z^a(a_{z,i}^s+b_z^a)\\\end{bmatrix}\Bigg)^2} \\
-#  &= -\frac{\partial}{\partial \alpha_{yz}}\bigg(\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2+\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)^2+\big(s_z^a(a_{z,i}^s+b_z^a)\big)^2\bigg) \\
-#  &= -\frac{\partial}{\partial \alpha_{yz}}\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2 \\
-#  &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial \alpha_{yz}}\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-#  &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial \alpha_{yz}}\big(\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)\big) \\
-#  &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\big(s_y^a(a_{y,i}^s+b_y^a)\big) \\
-# \end{align}$$
-# 
-# And then we do this for all the other variables
-# 
-# $$\begin{align}
-# %
-# % alpha_zy
-# %
-# \frac{\partial e_i}{\partial \alpha_{zy}} &= -\frac{\partial}{\partial \alpha_{zy}}\bigg(\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2+\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)^2+\big(s_z^a(a_{z,i}^s+b_z^a)\big)^2\bigg) \\
-# &= -\frac{\partial}{\partial \alpha_{zy}}\bigg(\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2\bigg) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial \alpha_{zy}}\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial \alpha_{zy}}\big(\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\big(s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# %
-# % alpha_zx
-# %
-# \frac{\partial e_i}{\partial \alpha_{zx}} &= -\frac{\partial}{\partial \alpha_{zx}}\bigg(\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2+\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)^2+\big(s_z^a(a_{z,i}^s+b_z^a)\big)^2\bigg) \\
-#  &= -\frac{\partial}{\partial \alpha_{zx}}\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)^2 \\
-#  &= -2\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial \alpha_{zx}}\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-#  &= -2\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial \alpha_{zx}}\big(-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-#  &= -2\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)\big(-s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# %
-# % s_x
-# %
-# \frac{\partial e_i}{\partial s_x^a} &= -\frac{\partial}{\partial s_x^a}\bigg(\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2\bigg) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial s_x^a}\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial
-# s_x^a}\big(s_x^a(a_{x,i}^s+b_x^a)\big) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial s_x^a}(a_{x,i}^s+b_x^a) \\
-# %
-# % s_y
-# %
-# \frac{\partial e_i}{\partial s_y^a} &= -\frac{\partial}{\partial s_y^a}\bigg(\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2+\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)^2\bigg) \\
-# &= -\frac{\partial}{\partial s_y^a}\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2+-\frac{\partial}{\partial s_y^a}\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)^2 \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial s_y^a}\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &\phantom{{}=1}+-2\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial s_y^a}\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial s_y^a}\big(-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)\big)+-2\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial s_y^a}\big(s_y^a(a_{y,i}^s+b_y^a)\big) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\big(-\alpha_{yz}(a_{y,i}^s+b_y^a)\big)+-2\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)(a_{y,i}^s+b_y^a) \\
-# %
-# % s_z
-# %
-# \frac{\partial e_i}{\partial s_z^a} &= -\frac{\partial}{\partial s_z^a}\bigg(\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2+\big(s_y^a(a_{y,i}^s+b_y^a)-
-# \alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)^2+\big(s_z^a(a_{z,i}^s+b_z^a)\big)^2\bigg) \\
-# &= -\frac{\partial}{\partial s_z^a}\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2+-\frac{\partial}{\partial s_z^a}\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)^2+-\frac{\partial}{\partial s_z^a}\big(s_z^a(a_{z,i}^s+b_z^a)\big)^2 \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial s_z^a}\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &\phantom{{}=1}+-2\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial s_z^a}\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &\phantom{{}=1}+-2\big(s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial s_z^a}\big(s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial s_z^a}\big(\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &\phantom{{}=1}+-2\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial s_z^a}\big(-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &\phantom{{}=1}+-2\big(s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial s_z^a}\big(s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\big(\alpha_{zy}(a_{z,i}^s+b_z^a)\big)+-2\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)\big(-\alpha_{zx}(a_{z,i}^s+b_z^a)\big) \\
-# &\phantom{{}=1}+-2\big(s_z^a(a_{z,i}^s+b_z^a)\big)(a_{z,i}^s+b_z^a) \\
-# %
-# % b_x
-# %
-# \frac{\partial e_i}{\partial b_x^a} &= -\frac{\partial}{\partial b_x^a}\bigg(\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2\bigg) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial b_x^a}\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial b_x^a}\big(s_x^a(a_{x,i}^s+b_x^a)\big) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)s_x^a \\
-# %
-# % b_y^a
-# %
-# \frac{\partial e_i}{\partial b_y^a} &= -\frac{\partial}{\partial b_y^a}\bigg(\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2+\big(s_y^a(a_{y,i}^s+b_y^a)-
-# \alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)^2\bigg) \\
-# &= -\frac{\partial}{\partial b_y^a}\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2+-\frac{\partial}{\partial b_y^a}\big(s_y^a(a_{y,i}^s+b_y^a)-
-# \alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)^2 \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial b_y^a}\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &\phantom{{}=1}+-2\big(s_y^a(a_{y,i}^s+b_y^a)-
-# \alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial b_y^a}\big(s_y^a(a_{y,i}^s+b_y^a)-
-# \alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial b_y^a}\big(-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a))\big)-2\big(s_y^a(a_{y,i}^s+b_y^a)-
-# \alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial b_y^a}\big(s_y^a(a_{y,i}^s+b_y^a)\big) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)(-\alpha_{yz}s_y^a)-2\big(s_y^a(a_{y,i}^s+b_y^a)-
-# \alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)s_y^a \\
-# %
-# % b_z
-# %
-# \frac{\partial e_i}{\partial b_z^a} &= -\frac{\partial}{\partial b_z^a}\bigg(\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2+\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)^2+\big(s_z^a(a_{z,i}^s+b_z^a)\big)^2\bigg) \\
-# &= -\frac{\partial}{\partial b_z^a}\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)^2 \\
-# &\phantom{{}=1}+-\frac{\partial}{\partial b_z^a}\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)^2 \\
-# &\phantom{{}=1}+-\frac{\partial}{\partial b_z^a}\big(s_z^a(a_{z,i}^s+b_z^a)\big)^2 \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial b_z^a}\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &\phantom{{}=1}+-2\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial b_z^a}\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &\phantom{{}=1}+-2\big(s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial b_z^a}\big(s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial b_z^a}\big(\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &\phantom{{}=1}+-2\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial b_z^a}\big(-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &\phantom{{}=1}+-2\big(s_z^a(a_{z,i}^s+b_z^a)\big)\frac{\partial}{\partial b_z^a}\big(s_z^a(a_{z,i}^s+b_z^a)\big) \\
-# &= -2\big(s_x^a(a_{x,i}^s+b_x^a)+-\alpha_{yz}s_y^a(a_{y,i}^s+b_y^a)+\alpha_{zy}s_z^a(a_{z,i}^s+b_z^a)\big)(\alpha_{zy}s_z^a)+-2\big(s_y^a(a_{y,i}^s+b_y^a)-\alpha_{zx}s_z^a(a_{z,i}^s+b_z^a)\big)(-\alpha_{zx}s_z^a)+-2\big(s_z^a(a_{z,i}^s+b_z^a)\big)s_z^a \\
-# \end{align}$$
+# Look in `docs/IMU_calibration.pdf`
 
-# References:
+# ## Evaluating the accuacy of numerical derivatives
+
+# In[41]:
+
+def func(theta_acc):
+    T = np.array([[1, -theta_acc[0], theta_acc[1]],[0, 1, -theta_acc[2]],[0,0,1]])
+    K = np.array([[theta_acc[3],0,0],[0,theta_acc[4],0],[0,0,theta_acc[5]]])
+    b = np.array([[theta_acc[6]],[theta_acc[7]],[theta_acc[8]]])
+    return 1 - np.linalg.norm(T@K@(a_i + b))**2
+
+def five_point_approximation(func, theta_acc, h):
+    N = theta_acc.shape[1]
+    partials = np.ndarray((N,1))
+    for partial_idx in range(N):
+        hs = np.array([-2*h, -h, h, 2*h])
+        nodes = np.repeat(theta_acc, 4, axis=0)
+        nodes[:,partial_idx] += hs
+        five_point_approx = 1/(12*h)*(func(nodes[0]) - 8*func(nodes[1]) + 8*func(nodes[2]) - func(nodes[3]))
+        partials[partial_idx] = five_point_approx
+    return partials
+
+
+# theta_acc = [a_yz, a_zy, a_zx, s_x_a, s_y_a, s_z_a, b_x_a, b_y_a, b_z_a]
+theta_acc = np.array([[0.1, 0, 0.1, 1, 1, 1, 0, 0.1, 0]], dtype=np.float32)
+a_i = np.array([[0.2], [0.7], [0.1]])
+h = 0.1
+
+print("five-point approximations")
+print(five_point_approximation(func, theta_acc, h))
+
+
+# hs = np.array([-2*h, -h, h, 2*h])
+# nodes = np.repeat(theta_acc, 4, axis=0)
+# nodes[:,0] += hsReferences:
 # 
 #  - https://www.math.uwaterloo.ca/~hwolkowi/matrixcookbook.pdf
 #  - http://eigen.tuxfamily.org/bz_attachmentbase/attachment.cgi?id=395
@@ -172,7 +92,7 @@ print("sample variance:", sample_variance)
 #  - https://medium.com/@sarvagya.vaish/levenberg-marquardt-optimization-part-2-5a71f7db27a0
 #  - https://github.com/SarvagyaVaish/Eigen-Levenberg-Marquardt-Optimization/blob/master/main.cpp
 
-# In[3]:
+# In[4]:
 
 data_filename = "recorded_sensor_data/imu_calibration_11_14_20-00-00/imu_calibration_data_11_14.csv"
 reader = csv.reader(open(data_filename, 'r'))
@@ -186,7 +106,7 @@ data = np.array(data)
 
 # ## iterate over the Tinit period to compute the gyro biases
 
-# In[4]:
+# In[5]:
 
 Tinit = 4
 samples_per_second = 100
--- python/recorded_data_processing/imu_calibration.py
+++ python/recorded_data_processing/imu_calibration.py
@@ -22,30 +22,13 @@ def main():
     data = np.array(data)
 
     # Example of LM optimization
-    theta_acc = autodiff.adnumber(np.zeros(9), 'theta_acc')
-    T = np.array([[1, -theta_acc[0], theta_acc[1]],
-                  [0, 1, -theta_acc[2]],
-                  [0, 0, 1]])
-    K = np.array([[theta_acc[3], 0, 0],
-                  [0, theta_acc[4], 0, 0],
-                  [0, 0, theta_acc[5]]])
-    b = np.array([[theta_acc[6], theta_acc[7], theta_acc[8]]])
-
-    # let c = a @ b
-    a = autodiff.adnumber(np.array([0, 0, 0]), 'a')
-    b = autodiff.adnumber(np.array([[2, 4, 1], [1, 5, 2], [4, 6, 3]]), 'b')
-    c = a * b + a  # confusingly, this does matrix multiplication
-    print(c.gradient([a]))
-
+    theta_acc = np.zeros(9, 1)
 
     def accelerometer_error_and_jacobian(a):
-        params = autodiff.adnumber(a, 'params')
-        f = 9.8 - T @ K @ (a - b)
-        df = params.gradient(theta_acc)
-        return f, df
+        return a, 1
 
-    # sol = root(accelerometer_error_and_jacobian, theta_acc, jac=True, method='lm')
-    # print(sol.x)
+    sol = root(accelerometer_error_and_jacobian, theta_acc, jac=True, method='lm')
+    print(sol.x)
 
 
 if __name__ == '__main__':
