5d141783714f06e2932acaac80f68886324aa37e
Peter Mitrano <mitranopeter@gmail.com>
2018-02-06 16:42:32 -0500

    marker detection analysis now goes to std::out
     - just use `>` to redirect to whatever file you want
     - add tvec and rvec

--- cpp/tools/marker_detection_analysis.cpp
+++ cpp/tools/marker_detection_analysis.cpp
@@ -5,15 +5,11 @@
 void detectMarkers(cv::VideoCapture capture,
                    const std::vector<unsigned long> &timestamps,
                    aruco::CameraParameters cam_params,
-                   const std::unordered_set<unsigned long> &ids,
                    bool step) {
   cv::Mat frame;
   cv::Mat annotated_frame;
 
   float marker_size = 0.158; // meters
-
-  // create output file stream
-  std::ofstream out_file("out_data.csv");
 
   //Create the detector
   aruco::MarkerDetector MDetector;
@@ -28,7 +24,6 @@ void detectMarkers(cv::VideoCapture capture,
     cam_params.resize(frame.size());
 
     size_t frame_idx = 0;
-    unsigned long last_detected_marker_timestamp = 0ul;
     std::unordered_map<int, unsigned int> detection_counts;
     while (capture.isOpened()) {
       capture >> frame;
@@ -43,23 +38,21 @@ void detectMarkers(cv::VideoCapture capture,
       std::vector<aruco::Marker> markers = MDetector.detect(frame);
 
       for (auto &marker : markers) {
-        // filter!
-        if (ids.count(static_cast<const unsigned long &>(marker.id)) != 0) {
-          tracker[marker.id].estimatePose(marker, cam_params, marker_size);
-          marker.draw(annotated_frame, cv::Scalar(0, 0, 255), 2);
+        tracker[marker.id].estimatePose(marker, cam_params, marker_size);
 
-          // draw the filtered tags that were detected
-          aruco::CvDrawingUtils::draw3dCube(annotated_frame, marker, cam_params);
-          aruco::CvDrawingUtils::draw3dAxis(annotated_frame, marker, cam_params);
+        // draw the tags that were detected
+        marker.draw(annotated_frame, cv::Scalar(0, 0, 255), 2);
+        aruco::CvDrawingUtils::draw3dCube(annotated_frame, marker, cam_params);
+        aruco::CvDrawingUtils::draw3dAxis(annotated_frame, marker, cam_params);
 
-          out_file << timestamps[frame_idx] << "," << marker.id << std::endl;
-        }
+        // output to std out so one can redirect to any file they want
+        std::cout << timestamps[frame_idx] << "," << marker.id << "," << marker.Tvec << "," << marker.Rvec << std::endl;
       }
 
-      cv::imshow("filtered annotated", annotated_frame);
-      cv::waitKey(10);
 
       if (step) {
+        cv::imshow("annotated", annotated_frame);
+        cv::waitKey(10);
         std::cin.get();
       }
 
@@ -72,7 +65,7 @@ void detectMarkers(cv::VideoCapture capture,
 void show_help();
 
 int main(int argc, char **argv) {
-  if (argc < 5) {
+  if (argc < 4) {
     show_help();
     return EXIT_FAILURE;
   }
@@ -80,7 +73,6 @@ int main(int argc, char **argv) {
   char *video_filename = argv[1];
   char *timestamps_filename = argv[2];
   char *params_filename = argv[3];
-  char *ids_filename = argv[4];
 
   cv::VideoCapture cap(video_filename);
   auto w = static_cast<const int>(cap.get(CV_CAP_PROP_FRAME_WIDTH));
@@ -134,60 +126,27 @@ int main(int argc, char **argv) {
     }
   }
 
-  // Read and process the file of IDs of tags to consider
-  std::ifstream ids_file(ids_filename);
-
-  if (!ids_file.good()) {
-    std::cout << "Bad file: [" << ids_filename << "]. " << std::strerror(errno) << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  std::unordered_set<unsigned long> ids;
-  while (!ids_file.eof()) {
-    std::string line;
-    ids_file >> line;
-
-    if (line.empty()) {
-      break;
-    }
-
-    size_t pos = 0;
-    try {
-
-      unsigned long id = std::stoul(line, &pos);
-      if (pos != line.length()) {
-        std::cout << "Error parsing whole line: [" << line << "] at " << pos << std::endl;
-      } else {
-        ids.insert(id);
-      }
-    }
-    catch (std::invalid_argument &e) {
-      std::cout << "Error parsing: [" << line << "]" << std::endl;
-      std::cout << e.what() << std::endl;
-    }
-  }
-
   bool step = false;
-  if (argc == 6) {
+  if (argc == 5) {
     step = true;
     std::cout << "Stepping (press enter for next frame)" << std::endl;
   }
 
-  detectMarkers(cap, timestamps, params, ids, step);
+  detectMarkers(cap, timestamps, params, step);
 }
 
 void show_help() {
-  std::cout << "USAGE: ./marker_detection_analysis video_file timer_stamps camera_params ids_to_filter [-s]"
+  std::cout << "USAGE: ./marker_detection_analysis video_file timer_stamps camera_params [-s]"
             << std::endl
             << std::endl
-            << "This program will create a file called out_data.csv, which is a csv of time stamp, tag id. "
+            << "This program will print detected tag info to standard out."
             << "You can analyze the results by feeding that file into analyze_marker_detection_analysis.py"
             << std::endl
             << std::endl
             << "you can use -s to step through frame by frame"
             << std::endl
-            << "Example: ./marker_detection_analysis input.avi timestamps.csv camera_params.yml ids.csv"
+            << "Example: ./marker_detection_analysis input.avi timestamps.csv camera_params.yml"
             << std::endl
-            << "         ./marker_detection_analysis input.avi timestamps.csv camera_params.yml ids.csv -s"
+            << "         ./marker_detection_analysis input.avi timestamps.csv camera_params.yml -s"
             << std::endl;
 }
