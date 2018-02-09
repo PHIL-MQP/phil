9c30948e91b78f9b53f2fdc8db5d60b39d65423c
Peter Mitrano <mitranopeter@gmail.com>
2018-02-09 13:35:07 -0500

    Demonstrate how to detect start time of chirp
     - looks like doppler effect won't mess up our range measurements by
     more than 2cm

--- matlab/chirp_filtering.m
+++ matlab/chirp_filtering.m
@@ -7,27 +7,50 @@ close all;
 %%
 
 % construct a series of chirps with the desired properties
-doppler = 0;
-F0 = 1000;
-F1 = 1000;
+F0 = 500;
+F1 = 2000;
+doppler = 0.01*F0;
 number_of_chirps = 1;
-T = 0:0.000001:0.01;
-c = chirp(T, F0 + doppler, T(end), F1 + doppler, 'linear', -90);
-noise = rand(1,5000)*0.05;
+dt = 0.000001;
+T = 0.02;
+noise_T = 0.02;
+t = 0:dt:T;
+t_total = 0:dt:T+2*noise_T;
+unshifted_chirp = chirp(t, F0, t(end), F1, 'linear', -90);
+c = chirp(t, F0, t(end), F1 + doppler, 'linear', -90);
+noise = rand(1,noise_T/dt)*0.05;
 padded_chirp = [noise c noise];
+unshifted_padded_chirp = [noise unshifted_chirp noise];
 signal = repmat(padded_chirp, 1, number_of_chirps);
+unshifted_signal = repmat(unshifted_padded_chirp, 1, number_of_chirps);
 
-filter = chirp(T, F0, T(end), F1, 'linear', -90);
+filter = chirp(t, F0, t(end), F1, 'linear', -90);
 flen = size(filter,2);
 
-y = zeros(size(signal,2) - flen);
-for i = (1:10:size(signal,2) - flen)
+y = zeros(1, size(signal,2) - flen);
+unshifted_y = zeros(1, size(signal,2) - flen);
+for i = (1:size(signal,2) - flen)
     window = signal(i:i+flen-1)';
-    y(i) = (filter * window) / 1000;
+    unshifted_window = unshifted_signal(i:i+flen-1)';
+    y(1, i) = (filter * window) / 1000;
+    unshifted_y(1, i) = (filter * unshifted_window) / 1000;
 end
 
 figure;
-hold on;
+subplot(3, 1, 1);
 plot(signal);
-% plot(filter);
-plot(y);
+plot(t_total, signal);
+xlim([t_total(1), t_total(end)]);
+title("signal");
+
+subplot(3, 1, 2);
+plot(t, filter, 'Color', 'r');
+xlim([t_total(1), t_total(end)]);
+title("filter");
+
+subplot(3, 1, 3);
+hold on;
+plot(t_total(1:end-flen), y);
+plot(t_total(1:end-flen), unshifted_y, 'Color', 'g');
+xlim([t_total(1), t_total(end)]);
+
