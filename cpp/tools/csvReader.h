9f5192244b004ca23ffe0e3c89e41f2ef3cba15b
Peter Mitrano <mitranopeter@gmail.com>
2018-02-11 02:32:40 -0500

    remove old unused code

--- cpp/tools/csvReader.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-std::vector<float> getData(std::string file_name, int row_number);
-
-std::vector< std::vector<float> > getData(std::string fileName, int startRow, int endRow);
-
-float myAtof ( std::string &num);
-
