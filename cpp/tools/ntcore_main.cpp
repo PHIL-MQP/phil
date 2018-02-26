a9fe2cd8c612601cbe5ac969df93a77aaa61826b
Peter Mitrano <mitranopeter@gmail.com>
2018-02-26 13:59:31 -0500

    make ntcore test script more useful

--- cpp/tools/ntcore_main.cpp
+++ cpp/tools/ntcore_main.cpp
@@ -5,14 +5,30 @@
 
 int main() {
   auto inst = nt::NetworkTableInstance::GetDefault();
+
   inst.StartClient("localhost", 1735);
 
+  std::cout << "Please start outline viewer in server mode.\n";
+
+  auto table = inst.GetTable("PhilTable");
+
+  std::cout << "Inserting the key value pair [NewKey:goodbye], check outline viewer\n";
+
+  constexpr auto key = "NewKey";
+  constexpr auto value = "goodbye";
+  table->PutString(key, value);
+
+  std::cout << "Retreiving that new key...\n";
+
   while (true) {
-    auto entry = inst.GetEntry("/Value");
+    auto entry = table->GetEntry(key);
     if (entry.Exists()) {
-      std::cout << entry.GetString("not found") << std::endl;
+      auto result = entry.GetString("not found");
+      assert(result == value);
+      std::cout << result << std::endl;
       break;
     }
   }
+
   return 0;
 }
