8ea3ba1fa396bc116a4bf3084770152569ee9836
Peter Mitrano <mitranopeter@gmail.com>
2018-02-28 02:33:32 -0500

    add plotting to fps analysis
     - added example plots to report

--- python/recorded_data_processing/fps_from_timestamps.py
+++ python/recorded_data_processing/fps_from_timestamps.py
@@ -2,21 +2,29 @@
 
 import numpy as np
 import argparse
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser("Compute FPS statistics from file of timestamps. Assumes microseconds")
     parser.add_argument("timestamps", help="The timestamps file output by mocap_record")
+    parser.add_argument("--no-plot", '-p', action="store_true", help="plot FPS over time")
     args = parser.parse_args()
 
     timestamps = np.genfromtxt(args.timestamps)
-    dt = timestamps[1:] - timestamps[0:-1]
-    mean_fps = 1000000 / np.mean(dt)
-    median_fps = 1000000 / np.median(dt)
-    min_fps = 1000000 / np.max(dt)
-    max_fps = 1000000 / np.min(dt)
+    dt = 1e6 / (timestamps[1:] - timestamps[0:-1])
+    mean_fps = np.mean(dt)
+    median_fps = np.median(dt)
+    max_fps = np.max(dt)
+    min_fps = np.min(dt)
     print("Parsing for:", args.timestamps);
-    print("Mean FPS:", mean_fps)
-    print("Median FPS:", median_fps)
-    print("Min FPS:", min_fps)
-    print("Max FPS:", max_fps)
+    print("mean,median,min,max")
+    print("{:0.2f}, {:0.2f}, {:0.2f}, {:0.2f}".format(mean_fps, median_fps, min_fps, max_fps))
+
+    if not args.no_plot:
+        plt.style.use("python/phil.mplstyle")
+        plt.figure()
+        plt.plot(dt)
+        plt.ylabel("time since last frame (fps)")
+        plt.xlabel("frames over time")
+        plt.show()
