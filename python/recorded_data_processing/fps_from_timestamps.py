a37117567bbdccca8be15289e30330e805bb2e36
Peter Mitrano <mitranopeter@gmail.com>
2018-03-18 12:59:30 -0400

    make fps script more robust to cwd

--- python/recorded_data_processing/fps_from_timestamps.py
+++ python/recorded_data_processing/fps_from_timestamps.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import numpy as np
 import argparse
 import matplotlib.pyplot as plt
@@ -11,20 +12,26 @@ if __name__ == "__main__":
     parser.add_argument("--no-plot", '-p', action="store_true", help="plot FPS over time")
     args = parser.parse_args()
 
+    if 'DISPLAY' not in os.environ:
+        matplotlib.use('Agg')
+
     timestamps = np.genfromtxt(args.timestamps)
     dt = 1e6 / (timestamps[1:] - timestamps[0:-1])
     mean_fps = np.mean(dt)
     median_fps = np.median(dt)
     max_fps = np.max(dt)
     min_fps = np.min(dt)
-    print("Parsing for:", args.timestamps);
+    print("Parsing:", args.timestamps);
     print("mean,median,min,max")
     print("{:0.2f}, {:0.2f}, {:0.2f}, {:0.2f}".format(mean_fps, median_fps, min_fps, max_fps))
 
     if not args.no_plot:
-        plt.style.use("python/phil.mplstyle")
+        recorded_data_processing_dir = os.path.dirname(os.path.realpath(__file__))
+        style = recorded_data_processing_dir + "/../phil.mplstyle"
+        plt.style.use(style)
         plt.figure()
-        plt.plot(dt)
-        plt.ylabel("time since last frame (fps)")
-        plt.xlabel("frames over time")
+        plt.plot(dt, linewidth=1)
+        plt.ylabel("FPS")
+        plt.xlabel("Frame Index")
+        plt.title("FPS Over Time")
         plt.show()
