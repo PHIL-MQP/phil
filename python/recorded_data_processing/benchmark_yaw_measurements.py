15d99fd3620cd3798c4100f1da88362b15d093c8
Peter Mitrano <mitranopeter@gmail.com>
2018-03-18 23:28:32 -0400

    finish yaw comparison code
     - wrapping of mocap is a real problem
    
    ./python/recorded_data_processing/benchmark_yaw_measurements.py recorded_sensor_data/turtlebot_mocap_3_17/trial_2/turtlebot_mocap_3_17\ 2.csv recorded_sensor_data/turtlebot_mocap_3_17/trial_2/rio-data-723.573.csv

--- python/recorded_data_processing/benchmark_yaw_measurements.py
+++ python/recorded_data_processing/benchmark_yaw_measurements.py
@@ -9,55 +9,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def main():
-    default_robot_name = 'Global Angle phil_turtlebot:phil_turtlebot'
-
-    parser = argparse.ArgumentParser("This script compares the yaw measurements of the fused navx reading, "
-                                     "naively integrated gyroscope, and motion capture.")
-    parser.add_argument('mocap_csv', help='csv motion capture file')
-    parser.add_argument('rio_csv', help='csv of navx data from the roborio')
-    parser.add_argument('--robot_name',
-                        help='title of the column with the rio data. default is [%s]'.format(default_robot_name),
-                        default=default_robot_name)
-    parser.add_argument("--no-plot", '-p', action="store_true", help="plot FPS over time")
-
-    args = parser.parse_args()
-
-    mocap_reader = csv.reader(open(args.mocap_csv, 'r'))
-    rio_reader = csv.reader(open(args.rio_csv, 'r'))
-
-    # MOCAP data
-    robot_poses = load_motion_capture_csv(args.robot_name, mocap_reader)
-    if robot_poses is None:
-        print("there's no column [{:s}] in [{:s}]".format(args.robot_name, args.mocap_csv))
-    mocap_yaws = robot_poses[:, 2] - robot_poses[0, 2]
-    mocap_yaws = np.rad2deg(mocap_yaws)
-    mocap_yaws = np.unwrap(mocap_yaws, discont=0.1)
-    mocap_times = np.arange(0, mocap_yaws.shape[0]) * 0.01
-
-    # RIO data
-    rio_data = load_rio_data(rio_reader)
-    navx_yaws = -rio_data[:, 8]
-    navx_yaws = np.unwrap(navx_yaws, discont=180)
-    navx_times = (rio_data[:, 18].astype(np.int32) - rio_data[0, 18]) / 1000
-
-    if not args.no_plot:
-        if 'DISPLAY' not in os.environ:
-            matplotlib.use('Agg')
-
-        recorded_data_processing_dir = os.path.dirname(os.path.realpath(__file__))
-        style = recorded_data_processing_dir + "/../phil.mplstyle"
-        plt.style.use(style)
-        plt.figure()
-        plt.plot(mocap_times, mocap_yaws, linewidth=1, label='motion capture')
-        plt.plot(navx_times, navx_yaws, linewidth=1, label='NavX getYaw()')
-        plt.ylabel("Yaw (degrees)")
-        plt.xlabel("Time (seconds)")
-        plt.title("Comparison of Yaw Measurement")
-        plt.legend()
-        plt.show()
-
-
 def load_motion_capture_csv(robot_name, mocap_reader):
     next(mocap_reader)
     next(mocap_reader)
@@ -90,6 +41,69 @@ def load_rio_data(rio_reader):
     for row in rio_reader:
         data.append([float(d) for d in row])
     return np.array(data)
+
+
+def integrate_gyro_z(gyro_z):
+    integrated_yaws = np.zeros(len(gyro_z))
+    yaw = 0
+    for i, g in enumerate(gyro_z):
+        yaw += g * 0.02
+        integrated_yaws[i] = yaw
+    return integrated_yaws
+
+
+def main():
+    default_robot_name = 'Global Angle phil_turtlebot:phil_turtlebot'
+
+    parser = argparse.ArgumentParser("This script compares the yaw measurements of the fused navx reading, "
+                                     "naively integrated gyroscope, and motion capture.")
+    parser.add_argument('mocap_csv', help='csv motion capture file')
+    parser.add_argument('rio_csv', help='csv of navx data from the roborio')
+    parser.add_argument('--robot_name',
+                        help='title of the column with the rio data. default is [%s]'.format(default_robot_name),
+                        default=default_robot_name)
+    parser.add_argument("--no-plot", '-p', action="store_true", help="plot FPS over time")
+
+    args = parser.parse_args()
+
+    mocap_reader = csv.reader(open(args.mocap_csv, 'r'))
+    rio_reader = csv.reader(open(args.rio_csv, 'r'))
+
+    # MOCAP data
+    robot_poses = load_motion_capture_csv(args.robot_name, mocap_reader)
+    if robot_poses is None:
+        print("there's no column [{:s}] in [{:s}]".format(args.robot_name, args.mocap_csv))
+    mocap_yaws = robot_poses[:, 2] - robot_poses[0, 2]
+    mocap_yaws = np.rad2deg(mocap_yaws)
+    mocap_yaws = np.unwrap(mocap_yaws, discont=0.1)
+    mocap_times = np.arange(0, mocap_yaws.shape[0]) * 0.01
+
+    # RIO data
+    rio_data = load_rio_data(rio_reader)
+    navx_yaws = -rio_data[:, 8]
+    navx_yaws = np.unwrap(navx_yaws, discont=180)
+    navx_times = (rio_data[:, 18].astype(np.int32) - rio_data[0, 18]) / 1000
+    gyro_z = rio_data[:, 5]
+
+    integrated_yaws = integrate_gyro_z(gyro_z)
+
+    if not args.no_plot:
+        if 'DISPLAY' not in os.environ:
+            matplotlib.use('Agg')
+
+        recorded_data_processing_dir = os.path.dirname(os.path.realpath(__file__))
+        style = recorded_data_processing_dir + "/../phil.mplstyle"
+        plt.style.use(style)
+        plt.figure()
+        plt.plot(mocap_times, mocap_yaws, linewidth=1, label='motion capture')
+        plt.plot(navx_times, navx_yaws, linewidth=1, label='NavX getYaw()')
+        plt.plot(navx_times, integrated_yaws, linewidth=1, label='Integrated Gyro '
+                                                                 'Z')
+        plt.ylabel("Yaw (degrees)")
+        plt.xlabel("Time (seconds)")
+        plt.title("Comparison of Yaw Measurement")
+        plt.legend()
+        plt.show()
 
 
 if __name__ == '__main__':
